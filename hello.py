55cefba782ce5d792809cabbc9e1da137e46ab6d
djalvaro <dalvaro@gmail.com>
2015-09-10 16:18:41 -0700

    more

--- hello.py
+++ hello.py
@@ -3,13 +3,10 @@ Created on Sep 8, 2015
 
 @author: PC-43
 '''
+'''
 print("hello World")
 
-<<<<<<< HEAD
-#  here is a Comment (though it doesn't say much).
-=======
 # here is a Comment
->>>>>>> 8054092d107a64c8a803702a588aac4ed8d069a6
 
 
 u=0
@@ -20,3 +17,49 @@ if u==0 or i<2:
     print("I am still in the if")
 else:
     print("U is not 0 and I is >2")
+    '''
+'''
+#def someFunction(a, b):
+#    print(a+b)
+#someFunction(12,451)
+
+
+
+def someFunction(a, b):
+    someFunction(12,451)
+    print(a+b)
+'''
+'''   
+a=10
+b=20
+def somefunction():
+    for z in (1,3, 5, 7, 9):
+        print (z)
+somefunction()
+
+def somefunction2():
+    for x in range (1,3, 5, 7, 9):
+        print (z)
+somefunction2()
+
+'''
+
+
+def somefunction():
+    mystring="i am HAPPY"
+    mystring.lower()
+    print(type(mystring))
+    b=(mystring.lower())
+    print(b)
+    
+somefunction()   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ mixedTypes.py
@@ -0,0 +1,36 @@
+'''
+Created on Sep 10, 2015
+
+@author: PC-43
+'''
+'''
+INTRUCTIONS:
+given a list of mixed types (ints and strings only),
+1. give the sum of ints in the list.
+2. create a new list of all the strings
+3. EXTRA - have the user input the list 
+
+PLAN:
+create list called mixedlist
+extract all ints from mixedlist into a new list called intslist
+add all list elements to make sumints
+extract all strings from mixedlist into a new list called stringslist
+
+'''
+
+def mixedtypelist():    
+    
+    mixedlist = [1,2,"glove","hat",15,"tomato"]
+        if 
+        
+
+    
+mixedtypelist()   
+
+
+
+
+
+
+
+

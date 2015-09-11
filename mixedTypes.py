ec5c908929f580f23c2301b62e142e3f772f9274
djalvaro <dalvaro@gmail.com>
2015-09-11 00:27:40 -0400

    solved with my input - next make user give input

--- mixedTypes.py
+++ mixedTypes.py
@@ -17,6 +17,12 @@ extract all ints from mixedlist into a new list called intslist
 add all list elements to make sumints
 extract all strings from mixedlist into a new list called stringslist
 
+NEXT
+ask user to input a list of only integers and/or strings, separated by commas
+if other than an int or str is input, print error and ask again - else go on to for loop
+user list then becomes mixedlist
+the rest should work from there.
+
 '''
 
 #def mixedtypelist():    
@@ -25,20 +31,17 @@ extract all strings from mixedlist into a new list called stringslist
 mixedlist = [1,2,"glove","hat",15,"tomato"] # here's my mixed-type list
 #print(mixedlist)                           # just checking - it prints
 intlist=0                       #starts at zero then a gets added to it each time through the for loop
-strlist=("")                    #i want a string that starts with nothing
+strlist=[]                    #i want to start with an empty list, then add each string that the for loop finds
 for a in (mixedlist):                       # for loop
-    #print(type(a))
-    if type(a) == int:                      #--if it's an int
-        #print("integer")                    #--just checking
+    #print(type(a))                        # just testing
+    if type(a) == int:                      #--if it's an int...
         intlist=a+intlist           #  1st integer + 0, then that gets added to next integer, etc.
-        #print (intlist)
-    elif type(a) == str:                    #--if it's a str
-        #print("string")                    #--just checking
-        strlist=(strlist+a+" ") # blank string added to first string added to next etc. but it's a long string, not a list!!
+    elif type(a) == str:                    #--if it's a str...
+        strlist.append(a)        #if it's a string, add it to strlist
     else:
         continue
-print(intlist)
-print(strlist)                  #now how do i convert this string into a list???
+print("The sum of all the integers is " + str(intlist))
+print("The list of all the strings is " + str(strlist))
 
 
 
